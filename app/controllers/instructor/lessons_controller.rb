8f7c026848d355e7d12ec72cb8e696da799db304
sierra mills <Alex49er13@gmail.com>
2019-06-09 04:49:35 +0000

    lesson permissions

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,16 +1,28 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_actioin :require_authorized_for_current_space
   def new 
-    @space = Space.find(params[:space_id])
     @lesson = Lesson.new
   end
 
-    def create
-    @space = Space.find(params[:space_id])
-    @lesson = @space.lessons.create(lesson_params)
-    redirect_to instructor_course_path(@space.course)
+  def create
+    @lesson = current_space.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_space.course)
   end
 
   private
+
+  def require_authorized_for_current_space
+    if current_space.course.user != current_user
+      return render plain: 'unauthorized', status: :unauthorized
+    end
+  end
+
+
+  helper_method :current_space
+  def Current_space
+    @current_space ||= Space.find(params[:space_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
