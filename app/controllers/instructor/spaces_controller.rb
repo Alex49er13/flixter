7f8caecf8662728543dec269f742943d5d501373
sierra mills <Alex49er13@gmail.com>
2019-06-11 00:52:31 +0000

    finalizing permissions

--- app/controllers/instructor/spaces_controller.rb
+++ app/controllers/instructor/spaces_controller.rb
@@ -1,15 +1,24 @@
 class Instructor::SpacesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
   def new
-    @course = Course.find(params[:course_id])
     @space = Space.new
   end
   def create
-    @course = Course.find(params[:course_id])
-    @space =@course.spaces.create(space_params)
-    redirect_to instructor_course_path(@course)
+    @space = @course.spaces.create(space_params)
+    redirect_to instructor_course_path(Current_course)
   end
 
   private
+
+  def requiree_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "unauthorized", status: :unauthorized
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
 
   def space_params
     params.require(:space).permit(:title)
