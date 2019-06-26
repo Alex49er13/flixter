f1e9e7dfd5da77cd3ff249a9adb6d83c2e1f483e
sierra.mills <alex49er13@gmail.com>
2019-06-26 02:29:45 +0000

    video added to lessons

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,8 +3,8 @@ class Course < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   belongs_to :user
-  has_many :sections
   has_many :spaces
+  has_many :enrollments
 
   validates :title,presence: true
   validates :description, presence: true
